7ad3be027f19941b28d7c02640578a4ef2c967c9
Francesca-Lisi <francescalisi2143@gmail.com>
2024-11-19 19:49:03 +0100

    feat: add function in Palindroma

--- PALINDROMA/script.js
+++ PALINDROMA/script.js
@@ -5,18 +5,17 @@ const parolaUtente = prompt('inserisci una parola').toLowerCase();
 let message;
 
 
-// function parolaPalindromaCheck (parolaUtente) {
+function parolaPalindromaCheck(parola) {
   let parolaReverse = ''; 
   for(let i = parolaUtente.length - 1; i >= 0; i-- ){
   parolaReverse += parolaUtente.at(i);
   }
 
-
-if (parolaUtente === parolaReverse) {
-  message = `La parola ${parolaUtente} è Palindroma`; 
-} else {
-  message = `La parola ${parolaUtente} NON è Palindroma`;
+  if (parolaUtente === parolaReverse) {
+  return `La parola "${parolaUtente}" è Palindroma`; 
+  } else {
+  return `La parola "${parolaUtente}" NON è Palindroma`;
+  }
 }
 
-
-console.log(message);
+console.log(parolaPalindromaCheck(parolaUtente));
