cce76e6ea105fe8547ccae45f66f14d94a3bdcfd
Francesca-Lisi <francescalisi2143@gmail.com>
2024-11-19 19:42:34 +0100

    feat: resolve exercise Palindroma without functions

--- PALINDROMA/script.js
+++ PALINDROMA/script.js
@@ -1,2 +1,22 @@
 //Chiedere all’utente di inserire una parola.
 //Creare una funzione per capire se la parola inserita è palindroma
+
+const parolaUtente = prompt('inserisci una parola').toLowerCase();
+let message;
+
+
+// function parolaPalindromaCheck (parolaUtente) {
+  let parolaReverse = ''; 
+  for(let i = parolaUtente.length - 1; i >= 0; i-- ){
+  parolaReverse += parolaUtente.at(i);
+  }
+
+
+if (parolaUtente === parolaReverse) {
+  message = `La parola ${parolaUtente} è Palindroma`; 
+} else {
+  message = `La parola ${parolaUtente} NON è Palindroma`;
+}
+
+
+console.log(message);
