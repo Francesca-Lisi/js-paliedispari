5c65f339ed13f269dc4b1509b0b082957680b249
Francesca-Lisi <francescalisi2143@gmail.com>
2024-11-19 21:19:35 +0100

    feat: add and valid prompt in Pari e Dispari

--- PARI E DISPARI/script.js
+++ PARI E DISPARI/script.js
@@ -3,3 +3,37 @@
 // Sommiamo i due numeri.
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+// PARI o DISPARI  GIOCATORE -----------------------------------
+
+let giocataPlayer;          
+let giocataValida = false;
+
+do {
+  giocataPlayer = prompt('Gioca PARI o DISPARI');
+  if(giocataPlayer.toUpperCase() === 'PARI' || giocataPlayer.toUpperCase() === 'DISPARI'){
+    giocataValida = true;
+  }
+} while (!giocataValida)
+
+console.log(giocataPlayer)
+
+// NUMERO DA 1 A 5 GIOCATORE ----------------------------------
+
+let numeroPlayer;          
+let isNumberValid = false;
+
+while(!isNumberValid){
+  numeroPlayer = parseInt(prompt('Inserisci un numero compreso fra 1 e 5'));
+  if(!isNaN(numeroPlayer) && numeroPlayer > 0 && numeroPlayer < 6){
+    isNumberValid = true;
+  }
+}
+
+console.log(numeroPlayer)
+
+// NUMERO DA 1 A 5 COMPUTER ------------------------------------
+
+const numeroComputer = Math.floor(Math.random() * 5) + 1;
+
+// Somma
